b492f2c8d18c629cae0b03d8d3b8532767ff25da
ali-sheikhali <ali.sheikhali1998@gmail.com>
2024-03-06 14:18:18 +0330

    reuseble components

--- src/components/api/MovieSection.js
+++ src/components/api/MovieSection.js
@@ -1,27 +1,8 @@
 import React from "react";
-import useSWR from "swr";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./TrendingMovies.css";
-
-const getBorderStyle = (voteAverage) => {
-  let borderColor;
-
-  if (voteAverage < 5) {
-    borderColor = "red";
-  } else if (voteAverage < 7) {
-    borderColor = "orange";
-  } else if (voteAverage < 9) {
-    borderColor = "green";
-  } else {
-    borderColor = "yellow";
-  }
-
-  return { borderColor, borderWidth: "4px", borderStyle: "solid" };
-};
-const fetcher = (...args) =>
-  fetch(...args).then((responsive) => responsive.json());
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -53,8 +34,22 @@ function SamplePrevArrow(props) {
     />
   );
 }
+const getBorderStyle = (voteAverage) => {
+  let borderColor;
 
-function TrendingMovies() {
+  if (voteAverage < 5) {
+    borderColor = "red";
+  } else if (voteAverage < 7) {
+    borderColor = "orange";
+  } else if (voteAverage < 9) {
+    borderColor = "green" ;
+  } else {
+    borderColor = "rgb(255,215,0)";
+  }
+
+  return { borderColor, borderWidth: "4px", borderStyle: "solid" };
+};
+function MovieSection({title,movies}) {
   const settings = {
     className: "center",
     centerMode: true,
@@ -86,24 +81,13 @@ function TrendingMovies() {
       },
     ],
   };
-  const apiKey = "bd422e7b500e20ac0bad0f395328407c";
-  const { data, error, isLoading } = useSWR(
-    `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${apiKey}`,
-    fetcher
-  );
-  if (error) {
-    return <h1>Not found</h1>;
-  }
-  if (isLoading) {
-    return <h1>loading...</h1>;
-  }
-  console.log(data);
+  
   return (
     <div className=" w-10/12 mx-auto slider-container my-10">
-      <h3 className="font-bold text-xl p-3">Trending</h3>
+      <h3 className="font-bold text-xl p-3">{title}</h3>
       <Slider {...settings}>
-        {data.results.map((movie) => (
-          <div className=" p-2 h-[420px]" key={movie.id}>
+        {movies.map((movie) => (
+          <div className=" p-3 h-[420px]" key={movie.id}>
             <div className="flex flex-col space-y-5 relative">
               <figure>
                 <img
@@ -112,7 +96,7 @@ function TrendingMovies() {
                   alt=""
                 />
                 <div
-                  className="absolute top-0 left-1 rounded-full p-1 bg-black text-white"
+                  className="absolute top-[-10px] left-1 rounded-full p-1 bg-black text-white"
                   style={getBorderStyle(movie.vote_average)}
                 >
                   {parseFloat(movie.vote_average).toFixed(1)}
@@ -120,9 +104,9 @@ function TrendingMovies() {
               </figure>
               <div className="px-2">
                 <p className="font-bold hover:text-[rgba(1,180,228,1)] ">
-                  {movie.title}
+                  {movie.title ? movie.title : movie.original_name }
                 </p>
-                <p>{movie.release_date}</p>
+                <p>{movie.release_date ? movie.release_date : movie.first_air_date}</p>
               </div>
             </div>
           </div>
@@ -132,4 +116,4 @@ function TrendingMovies() {
   );
 }
 
-export default TrendingMovies;
+export default MovieSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import HeroHeader from '../components/HeroHeader'
-import TrendingMovies from '../components/api/TrendingMovies'
+import React from "react";
+import HeroHeader from "../components/HeroHeader";
+import MovieSection from "../components/api/MovieSection";
+import useSWR from "swr";
+
+const fetcher = (...args) =>
+  fetch(...args).then((responsive) => responsive.json());
 
 function Home() {
+  const apiKey = "bd422e7b500e20ac0bad0f395328407c";
+
+  const { data: trendingMovie, error: trendingMovieError,isLoading: trendingMovieIsLoading } = useSWR(
+    `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`,
+    fetcher
+  );
+  const {data: popularMovie,error: popularMovieError,isLoading: popularMovieIsLoading } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=${apiKey}`,
+    fetcher
+  );
+  if (trendingMovieError || popularMovieError) {
+    return <h1>Not found</h1>;
+  }
+  if (trendingMovieIsLoading || popularMovieIsLoading) {
+    return <h1>loading...</h1>;
+  }
+
   return (
     <div>
       <HeroHeader />
-      <TrendingMovies />
+      <MovieSection title="trending" movies={trendingMovie.results} />
+      <MovieSection title="What's Popular" movies={popularMovie.results} />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
